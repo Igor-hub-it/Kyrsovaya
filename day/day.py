07b2e1c90e2923b13e368f95f4f2f7933a0345cf
Igor-hub-it <79980006+Igor-hub-it@users.noreply.github.com>
2021-06-08 00:09:18 +1000

    kyrsach

--- day/day.py
+++
@@ -1,9 +0,0 @@
-class Day:
-    class State:
-       def __init__(self, stmth):
-           self._smth = _smth
-
-    def __init__(self, id):
-        self.id = id
-        
-test = Day.State("wfjo")
