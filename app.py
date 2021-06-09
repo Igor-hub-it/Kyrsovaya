618c867f4da3a6156f3f5bd65581d63d9150ac01
Igor-hub-it <79980006+Igor-hub-it@users.noreply.github.com>
2021-06-09 21:25:00 +1000

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+# from jinja2 import Environment
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('includs.html')
 
 if __name__ == "__main__":
     app.run()
+
