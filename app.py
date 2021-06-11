9998d8e7518ec53631462ecb7bd64d909aa27f17
Igor-hub-it <79980006+Igor-hub-it@users.noreply.github.com>
2021-06-11 16:07:57 +1000

    commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect, request
 # from jinja2 import Environment
+from datetime import datetime, date
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/',)
+def includs():
     return render_template('includs.html')
 
 if __name__ == "__main__":
     app.run()
-
