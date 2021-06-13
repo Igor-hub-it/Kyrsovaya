f7908b9156eb9fd7961dc91ef775149bc60c95d1
Artyom Erofeev <34557625+ErofeevAA@users.noreply.github.com>
2021-06-14 01:30:31 +1000

    Delete time.js

--- static/time.js
+++
@@ -1,7 +0,0 @@
-function liveTime() {   
-    let live_time = new Date().toLocaleString()
-    time.textContent = 'Сейчас: ' + live_time/*.slice(live_time.indexOf(',') + 2)*/
-}
-
-const time = document.querySelector('.first_block .date')
-setInterval(liveTime, 1000)
