669fb3bc4780474f66be0caa4d606735cac6b678
erofeevAA <erofeev.aa@students.dvfu.ru>
2021-06-23 11:15:48 +1000

    upload logon.py

--- form/logon.py
+++ form/logon.py
@@ -4,12 +4,15 @@ from wtforms.validators import InputRequired, Length
 
 
 class LoginForm(FlaskForm):
-    login = StringField('username', validators=[InputRequired(), Length(min=4, max=16)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
+    login = StringField('Логин', validators=[InputRequired(), Length(min=4, max=16, message="От 4 до 16 символов")])
+    password = PasswordField('Пароль', validators=[InputRequired(),
+                                                   Length(min=8, max=80, message="От 8 до 80 символов")])
+    remember = BooleanField('Запомнить меня')
 
 
 class RegisterForm(FlaskForm):
-    login = StringField('Логин:', validators=[InputRequired(), Length(min=4, max=16)])
-    password = PasswordField('Пароль:', validators=[InputRequired(), Length(min=8, max=80)])
-    vk_link = StringField('Ссылка на профиль Вконтакте:', validators=[InputRequired(), Length(min=7, max=31)])
+    login = StringField('Логин:', validators=[InputRequired(), Length(min=4, max=16, message="От 4 до 16 символов")])
+    password = PasswordField('Пароль:', validators=[InputRequired(),
+                                                    Length(min=8, max=80, message="От 8 до 80 символов")])
+    vk_link = StringField('Ссылка на профиль Вконтакте:',
+                          validators=[InputRequired(), Length(min=7, max=31, message="От 7 до 31 символов")])
